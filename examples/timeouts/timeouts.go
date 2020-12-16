fb430f8e76daabf47fe75f52424a34df692c54f4
root <root@DESKTOP-08E761I.localdomain>
2020-12-16 09:48:02 +0800

    initial...

---
+++ examples/timeouts/timeouts.go
@@ -0,0 +1,60 @@
+// _Timeouts_ are important for programs that connect to
+// external resources or that otherwise need to bound
+// execution time. Implementing timeouts in Go is easy and
+// elegant thanks to channels and `select`.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// For our example, suppose we're executing an external
+	// call that returns its result on a channel `c1`
+	// after 2s. Note that the channel is buffered, so the
+	// send in the goroutine is nonblocking. This is a
+	// common pattern to prevent goroutine leaks in case the
+	// channel is never read.
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	// Here's the `select` implementing a timeout.
+	// `res := <-c1` awaits the result and `<-time.After`
+	// awaits a value to be sent after the timeout of
+	// 1s. Since `select` proceeds with the first
+	// receive that's ready, we'll take the timeout case
+	// if the operation takes more than the allowed 1s.
+	var start int = time.Now().Nanosecond()
+	fmt.Println(time.Now())
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(2 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	// If we allow a longer timeout of 3s, then the receive
+	// from `c2` will succeed and we'll print the result.
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(9 * time.Second)
+		c2 <- "result 2"
+	}()
+
+	var end int = time.Now().Nanosecond()
+	fmt.Println(time.Now())
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(7 * time.Second):
+		fmt.Println("timeout 2")
+	}
+	fmt.Println(end - start)
+	fmt.Println(time.Now())
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module tools
+
+go 1.14
+
+require (
+	github.com/graphql-go/graphql v0.7.9 // indirect
+	github.com/russross/blackfriday v1.5.2
+	golang.org/x/tools v0.0.0-20201021000207-d49c4edd7d96 // indirect
+)
